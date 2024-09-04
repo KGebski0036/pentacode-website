82f78969ed29712ca9195ea3ddbb8118211d5fe3
ADRIAN\adria <adrian.dom28033@gmail.com>
2024-09-04 16:31:07 +0200

    added projects card and footer component

--- pentacode-website/src/app/pages/project-page/project-page.component.ts
+++ pentacode-website/src/app/pages/project-page/project-page.component.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NavigationBarComponent } from "../../components/navigation-bar/navigation-bar.component";
+import { Card1Component } from "../../components/card1/card1.component";
+import { ProjectCardComponent } from "../../components/project-card/project-card.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
 	selector: 'app-project-page',
 	standalone: true,
 	imports: [
     CommonModule,
-    NavigationBarComponent
+    NavigationBarComponent,
+    Card1Component,
+    ProjectCardComponent,
+    FooterComponent
 ],
 	templateUrl: './project-page.component.html',
 	styleUrl: './project-page.component.css',
