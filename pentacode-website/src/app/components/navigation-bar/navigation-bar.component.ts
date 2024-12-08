b0f4cd75ae9b28b2d00a7a32035a06ade7ae1c46
Jacek Jarosz <jacekjarosz55@gmail.com>
2024-12-08 17:26:26 +0100

    move nav to root

--- pentacode-website/src/app/components/navigation-bar/navigation-bar.component.ts
+++ pentacode-website/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-navigation-bar',
 	standalone: true,
 	imports: [
 		CommonModule,
+    RouterLink
 	],
 	templateUrl: './navigation-bar.component.html',
 	styleUrl: './navigation-bar.component.css',
