e39007e417d8a709a7a4cfb0e1b0165aa947c116
Jacek Jarosz <jacekjarosz55@gmail.com>
2024-12-11 20:24:06 +0100

    change about us

--- pentacode-website/src/app/pages/about-us-page/about-us-page.component.ts
+++ pentacode-website/src/app/pages/about-us-page/about-us-page.component.ts
@@ -23,34 +23,42 @@ export class AboutUsPageComponent {
     {
       firstName: "Karol",
       lastName:  "Gębski",
-      skills: ["C#", "C++"],
+      skills: ["C#", "C++", "Linux", "Cybersecurity"],
       aboutUrl: "#",
       photoUrl: "team/karol.jpg",
-      description: "nwm jest zbombiony"
+      description: "Co-founder, Leader of back-end developement. Experienced in Linux Administration and Cybersecurity"
     },
     {
-      firstName: "Marcin",
-      lastName:  "Furmańczyk",
-      skills: ["C#", "C++", "Project Management", "Crisis Management", "HR", "Scrum Master"],
-      aboutUrl: "#",
-      photoUrl: "team/marcin.jpg",
-      description: "Zjada kolegów :("
-    },
-    {
-      firstName: "Oskar",
-      lastName:  "Malarecki",
-      skills: ["Angular", "C#", "React", "HTML/CSS"],
-      aboutUrl: "#",
-      photoUrl: "team/oskar.jpg",
-      description: "Lubi musztardę i Angular."
-    }, {
       firstName: "Jacek",
       lastName:  "Jarosz",
       skills: ["Angular", "C#", "HTML/CSS", "Linux"],
       aboutUrl: "#",
       photoUrl: "team/jaca.jpg",
-      description: ""
+      description: "Co-founder, Leader of front-end Developement"
     },
+    {
+      firstName: "Marcin",
+      lastName:  "Furmańczyk",
+      skills: ["C#", "Project Management"],
+      aboutUrl: "#",
+      photoUrl: "team/marcin.jpg",
+      description: "Co-founder, Project manager and .NET developer."
+    },     {
+      firstName: "Oskar",
+      lastName:  "Malarecki",
+      skills: ["Angular", "HTML/CSS"],
+      aboutUrl: "#",
+      photoUrl: "team/oskar.jpg",
+      description: "Front-end Developer"
+    },   
+    {
+      firstName: "Mateusz",
+      lastName:  "Szymkiewicz",
+      skills: ["C#"],
+      aboutUrl: "#",
+      photoUrl: "team/empty.svg",
+      description: "Back-end Developer"
+    }
   ];
 
 
